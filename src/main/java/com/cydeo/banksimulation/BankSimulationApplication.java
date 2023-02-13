cab624db6254d57f13047a7d9baf71d1a11b8f0c
nijat <nijat.de@gmail.com>
2023-02-13 20:32:04 +0100

    security part controller, service, repository

--- src/main/java/com/cydeo/banksimulation/BankSimulationApplication.java
+++ src/main/java/com/cydeo/banksimulation/BankSimulationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
@@ -18,6 +20,11 @@ public class BankSimulationApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
